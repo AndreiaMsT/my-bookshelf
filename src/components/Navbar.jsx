ac8fc0cb4fd8b97dcf44b50934fb28cf6d1c2485
AndreiaMsT <am.teixeira4@gmail.com>
2024-03-10 17:54:22 +0000

    Done Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import pileOfBooks from "../assets/books.svg";
+import goodreadsIcon from "../assets/goodreads-icon.svg";
+
+export const Navbar = () => {
+    const getViewBox = () => {
+        if (window.innerWidth < 640) {
+          return "0 0 900 320"; // for small devices
+        } else {
+          return "0 80 1440 320"; // for larger devices
+        }
+      };
+  return (
+    <div className="relative wave-header">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox={getViewBox()}>
+        <path
+          fill="#ff5500"
+          fillOpacity="0.1"
+          d="M0,128L60,144C120,160,240,192,360,192C480,192,600,160,720,144C840,128,960,128,1080,133.3C1200,139,1320,149,1380,154.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+        ></path>
+      </svg>
+      <nav className="relative z-10 w-full flex justify-between items-center p-4">
+        <div className="flex items-center">
+          <p className="flex items-center">
+            MyBookshelf{" "}
+            <img
+              src={pileOfBooks}
+              className="h-[30px] w-[30px] ml-2"
+              alt="pile of books"
+            />
+          </p>
+        </div>
+        <a
+          href="https://www.goodreads.com/user/show/135436782-andreia-t"
+          className="relative z-10"
+          target="_blank"
+        >
+          <img
+            src={goodreadsIcon}
+            className="h-[30px] w-[30px]"
+            alt="goodreads icon"
+          />
+        </a>
+      </nav>
+    </div>
+  );
+};
